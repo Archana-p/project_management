c0d3a0ee66e2d4e1040a2220be38238bcd583724
Archana <archana10dpatil@gmail.com>
2014-09-26 16:18:07 +0530

    set the log of the ruby to rails log

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,3 +65,5 @@ ProjectManagement::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 end
+ENV['OPENSHIFT_RUBY_LOGDIR'] = Rails.join('log/production.log').to_s
+
