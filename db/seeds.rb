f9334e3104576c237a85a760840ceef09358a3e5
Archana <archana10dpatil@gmail.com>
2014-09-30 15:27:58 +0530

    add users to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
+	# This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
 # Examples:
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+    User.create(email:"mayur@gmail.com",name:"mayur",password:"mayur123",password_confirmation:"mayur123")
+    User.create(email:"minakshi@gmail.com",name:"minakshi",password:"minakshi123",password_confirmation:"minakshi123")
+    User.create(email:"arun@gmail.com",name:"arun",password:"arun1234",password_confirmation:"arun1234")
+    User.create(email:"viren@gmail.com",name:"viren",password:"viren123",password_confirmation:"viren123")
+    User.create(email:"pandurang@gmail.com",name:"pandurang",password:"pandurang123",password_confirmation:"pandurang123")
